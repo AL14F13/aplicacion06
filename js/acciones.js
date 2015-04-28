06aadaec168efdb30b3b8ad1c19e8d1f9f737598
AL14F13 <BotkkaBela24@gmail.com>
2015-04-28 13:36:43 -0500

    dos
    se agrego el swipe

--- js/acciones.js
+++ js/acciones.js
@@ -1,7 +1,21 @@
 // JavaScript Document
 $(document).ready(function(e) {
 document.addEventListener("deviceready",function(){
-	
+ $('btnvibrar').on('tap',function(){
+	 navigators.vibrate(1000);
+ });//tap de vibrar
+  $('btnbeep').on('tap',function(){
+	 navigator.beep(1);
+ });//tap de beep 
+ $('#izquierda').on('swipeleft',function(){
+	 alert("barrido a la izquierda");
+ });//izquierda
+  $('#derecha').on('swiperight',function(){
+	 alert("barrido a la derecha");
+ });//derecha
+ 
+  
+  
 }); 
 });
 
